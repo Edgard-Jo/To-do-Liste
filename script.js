a1376c32f836bda5e1e1c94e50d2eec32fc8426c
Mohamad <mohamad.alhajr@dci-student.org>
2024-04-08 15:05:14 +0200

    edit  html

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 const inputBox = document.getElementById("input-Box");
 const listContainer = document.getElementById("List-Container");
+const addButton = document.getElementById("add-button");
 
-function addToDoList () {
-    if (inputBox.value === "") {
-        alert("Du musst was rein schreiben!");
-        
-    } else {
-        let li = document.createElement("li");
-        li.innerHTML = inputBox.value;
-        listContainer.appendChild(li);
-    }
+addButton.addEventListener("click", addTask);
+
+function addTask() {
+  if (inputBox.value === "") {
+    alert("Du musst was rein schreiben!");
+  } else {
+    let li = document.createElement("li");
+    li.innerHTML = inputBox.value;
+    listContainer.appendChild(li);
+  }
 }
