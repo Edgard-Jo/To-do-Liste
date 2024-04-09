fb9b123d514e8ce377261665336fabbc075feb02
Patrick <patrick.roessler@dci-student.org>
2024-04-09 13:39:18 +0200

    update uhrzeit

--- index.js
+++ index.js
@@ -2,30 +2,7 @@ const inputBox = document.getElementById("input-box");
 const listContainer = document.getElementById("list-container");
 
 function addToDoList() {
-<<<<<<< HEAD
-    const inputValue = inputBox.value.trim();
-    
-    if (inputValue === "") {
-        alert("Du musst etwas eingeben!");
-    } else {
-        const li = document.createElement("li");
-        li.textContent = inputValue;
-        
-        // Erstellen des Löschbuttons
-        const deleteButton = document.createElement("span");
-        deleteButton.innerHTML = "&times;"; // Das 'x'-Symbol für den Löschbutton
-        deleteButton.className = "delete-btn";
-        deleteButton.addEventListener("click", function() {
-            li.remove(); // Entfernen des li-Elements beim Klicken auf den Löschbutton
-        });
-        
-        li.appendChild(deleteButton); // Hinzufügen des Löschbuttons zum li-Element
-        listContainer.appendChild(li);
-        inputBox.value = "";
-        inputBox.value = ""; // Clear input box after adding task
-=======
   const inputValue = inputBox.value.trim(); // Trimmen, um Leerzeichen zu entfernen
->>>>>>> 1942a0b0689f2a689938696572822734c0720681
 
   if (inputValue === "") {
     alert("Du musst etwas eingeben!");
@@ -49,15 +26,6 @@ function addToDoList() {
   }
 }
 
-<<<<<<< HEAD
-
-inputBox.addEventListener("keydown", function(event) {  
-   
-    if (event.key === "Enter") {   // Überprüfen, ob die gedrückte Taste die Enter-Taste ist 
-       // Aufrufen der addToDoList()-Funktion, wenn die Enter-Taste gedrückt wird
-        addToDoList();
-    }
-=======
 inputBox.addEventListener("keydown", function (event) {
   if (event.key === "Enter") {
     // Überprüfen, ob die gedrückte Taste die Enter-Taste ist
@@ -74,5 +42,21 @@ listContainer.parentElement.addEventListener("click", function (e) {
     e.target.parentElement.remove();
     saveData();
   }
->>>>>>> 1942a0b0689f2a689938696572822734c0720681
 });
+
+const currentTimeFooter = document.getElementById("current-time-footer");
+
+function updateCurrentTimeFooter() {
+  const now = new Date();
+  const hours = now.getHours().toString().padStart(2, "0");
+  const minutes = now.getMinutes().toString().padStart(2, "0");
+  const seconds = now.getSeconds().toString().padStart(2, "0");
+  currentTimeFooter.textContent = `Current Time: ${hours}:${minutes}:${seconds}`;
+}
+
+// Aktualisieren Sie die Uhrzeit alle Sekunde
+setInterval(updateCurrentTimeFooter, 1000);
+
+// Rufen Sie die Funktion einmal auf, um die Uhrzeit sofort anzuzeigen
+updateCurrentTimeFooter();
+
