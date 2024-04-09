dda883e174107d041f12b6e009f033684408b5de
Mohamad <mohamad.alhajr@dci-student.org>
2024-04-09 12:00:45 +0200

    edit  html

--- index.js
+++ index.js
@@ -2,34 +2,44 @@ const inputBox = document.getElementById("input-box");
 const listContainer = document.getElementById("list-container");
 
 function addToDoList() {
-    const inputValue = inputBox.value.trim(); // Trimmen, um Leerzeichen zu entfernen
-    
-    if (inputValue === "") {
-        alert("Du musst etwas eingeben!");
-    } else {
-        const li = document.createElement("li");
-        li.textContent = inputValue;
-        
-        // Erstellen des Löschbuttons
-        const deleteButton = document.createElement("span");
-        deleteButton.innerHTML = "&times;"; // Das 'x'-Symbol für den Löschbutton
-        deleteButton.className = "delete-btn";
-        deleteButton.addEventListener("click", function() {
-            li.remove(); // Entfernen des li-Elements beim Klicken auf den Löschbutton
-        });
-        
-        li.appendChild(deleteButton); // Hinzufügen des Löschbuttons zum li-Element
-        listContainer.appendChild(li);
-        inputBox.value = ""; // Clear input box after adding task
+  const inputValue = inputBox.value.trim(); // Trimmen, um Leerzeichen zu entfernen
 
-        inputBox.focus(); // put focus back to Inout box
-    }
+  if (inputValue === "") {
+    alert("Du musst etwas eingeben!");
+  } else {
+    const li = document.createElement("li");
+    li.textContent = inputValue;
+
+    // Erstellen des Löschbuttons
+    const deleteButton = document.createElement("span");
+    deleteButton.innerHTML = "&times;"; // Das 'x'-Symbol für den Löschbutton
+    deleteButton.className = "delete-btn";
+    deleteButton.addEventListener("click", function () {
+      li.remove(); // Entfernen des li-Elements beim Klicken auf den Löschbutton
+    });
+
+    li.appendChild(deleteButton); // Hinzufügen des Löschbuttons zum li-Element
+    listContainer.appendChild(li);
+    inputBox.value = ""; // Clear input box after adding task
+
+    inputBox.focus(); // put focus back to Inout box
+  }
 }
 
-inputBox.addEventListener("keydown", function(event) {  
-   
-    if (event.key === "Enter") {   // Überprüfen, ob die gedrückte Taste die Enter-Taste ist 
-       // Aufrufen der addToDoList()-Funktion, wenn die Enter-Taste gedrückt wird
-        addToDoList();
-    }
+inputBox.addEventListener("keydown", function (event) {
+  if (event.key === "Enter") {
+    // Überprüfen, ob die gedrückte Taste die Enter-Taste ist
+    // Aufrufen der addToDoList()-Funktion, wenn die Enter-Taste gedrückt wird
+    addToDoList();
+  }
+});
+
+listContainer.parentElement.addEventListener("click", function (e) {
+  if (e.target.tagName === "LI") {
+    e.target.classList.toggle("checked");
+    saveData();
+  } else if (e.target.tagName === "SPAN") {
+    e.target.parentElement.remove();
+    saveData();
+  }
 });
